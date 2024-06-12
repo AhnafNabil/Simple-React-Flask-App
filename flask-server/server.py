9ff8e253ef7a38d8e3be51819168bf8fa8c039aa
Ahnaf Tajwear Nabil <ahnafnabil14@gmail.com>
2024-06-12 22:38:39 +0600

    Files uploaded

---
+++ flask-server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+
+# Members API Route
+@app.route("/members")
+def members():
+    return jsonify({"members": ["Member1", "Member2", "Member3"]})
+
+
+if __name__=="__main__":
+    app.run(debug=True)
